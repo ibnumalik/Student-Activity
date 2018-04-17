951687362c2ad929bb745c5ac5f93eb049412f06
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-04-17 19:43:53 +0800

    Make API requet to backend for login authenticate

--- resources/js/auth/components/login/login.component.ts
+++ resources/js/auth/components/login/login.component.ts
@@ -1,8 +1,33 @@
 export class LoginComponent implements ng.IComponentOptions {
   static NAME:string = 'appLogin';
   template: any;
+  controllerAs:string;
+  controller;
 
   constructor() {
     this.template = require('./login.component.html');
+    this.controllerAs = '$ctrl';
+    this.controller = LoginController;
+  }
+}
+
+class LoginController implements ng.IComponentController {
+  static $inject = ['$http'];
+  title: string;
+  user;
+
+  constructor(private http: ng.IHttpService) {
+    this.title = "Login Page";
+    this.user = {};
+  }
+
+  login() {
+    this.http.post('http://local.studentapi.com/api/login', this.user, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      }
+    }).then(response => {
+      console.log(response);
+    });
   }
 }
