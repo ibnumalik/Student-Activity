fd0576a41460063a1078ab807ab020714e9ef798
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-04-18 15:05:21 +0800

    Ask css-loader plugin to ignore url handling

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,10 @@ module.exports = {
             loader: 'style-loader'
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
           },
           {
             loader: 'sass-loader'
