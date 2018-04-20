8605b8ee312783766ddabb1c49237e517b5559db
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-04-20 18:40:10 +0800

    Send data using jqaram in registration controller

--- resources/app/auth/components/register/register.component.ts
+++ resources/app/auth/components/register/register.component.ts
@@ -12,22 +12,30 @@ export class RegisterComponent implements ng.IComponentOptions {
 }
 
 class RegisterController implements ng.IComponentController {
-  static $inject = ['$http', '$state'];
+  static $inject = ['$http', '$state', '$httpParamSerializerJQLike'];
   title: string;
   user;
   emailUsed = false;
 
-  constructor(private http: ng.IHttpService, private $state: ng.ui.IStateService) {
+  constructor(
+    private http: ng.IHttpService,
+    private $state: ng.ui.IStateService,
+    private httpParamSerializerJQLike: ng.IHttpParamSerializer
+  ) {
     this.title = "Register Page";
     this.user = {};
   }
 
   register() {
-    this.http.post('http://localhost:8080/api/register', this.user, {
+    this.http.post(
+      'http://localhost:8080/api/register',
+      this.httpParamSerializerJQLike(this.user),
+      {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
-    }).then(response => {
+      }
+    ).then(response => {
       console.log(response.data);
 
       if (response.data === 'email already used') {
