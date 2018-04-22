1c0dcad0d5c03971c63092fa5683aed0cd940bc6
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-04-22 15:11:05 +0800

    Add ng-annotate-loader to fix ng inject error

--- webpack.config.js
+++ webpack.config.js
@@ -3,84 +3,97 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: [
-      './resources/app/index.ts'
-    ]
-  },
-  context: __dirname + '',
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/ ,
-          name: 'vendor',
-          chunks: 'all'
+    context: path.resolve(__dirname),
+
+    entry: {
+        app: [
+            './resources/app/index.ts'
+        ]
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
         }
-      }
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              url: false
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'ng-annotate-loader',
+                    'ts-loader',
+                ]
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]'
+                        }
+                    }
+                ]
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
             }
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
-      }
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        })
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html')
-    })
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    compress: true,
-    port: 8008
-  }
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 8008
+    }
 };
