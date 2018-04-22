3e5be1006ec396807cbc9f5648f005b0be1d5283
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-04-22 22:21:20 +0800

    Change navbar items based on user auth state

--- resources/app/core/components/app/app.component.ts
+++ resources/app/core/components/app/app.component.ts
@@ -1,10 +1,24 @@
+import { AuthService, IAuthService } from './../../../auth/services/auth.service';
 import './app.component.scss';
 
 export class App implements ng.IComponentOptions {
   static NAME:string = 'app';
   template;
+  controller;
 
   constructor() {
     this.template = require('./app.component.html');
+    this.controller = AppController;
   }
+}
+
+export class AppController implements ng.IComponentController{
+  public loggedIn: boolean;
+
+  constructor(private AuthService: IAuthService) {}
+
+  $doCheck() {
+    this.loggedIn = this.AuthService.isLoggedIn();
+  }
+
 }
--- resources/app/core/components/header/nav.component.ts
+++ resources/app/core/components/header/nav.component.ts
@@ -1,10 +1,20 @@
 import './nav.component.scss';
 
 export class NavbarComponent implements ng.IComponentOptions {
-  static NAME:string = 'navbar';
-  template:any;
+  static NAME: string = 'navbar';
+  bindings;
+  template: any;
+  controller: any;
 
   constructor() {
     this.template = require('./nav.component.html');
+    this.controller = NavbarComponentController;
+    this.bindings = { loggedIn: '<' }
   }
+}
+
+export class NavbarComponentController implements ng.IComponentController {
+  public loggedIn;
+
+  constructor() {}
 }
