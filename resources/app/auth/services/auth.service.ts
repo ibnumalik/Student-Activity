5a7bb894e1a132e01ce63f75d7f6ac04eb46c2d1
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-04-26 17:12:21 +0800

    Add user login method to AuthService

--- resources/app/auth/services/auth.service.ts
+++ resources/app/auth/services/auth.service.ts
@@ -4,20 +4,42 @@ export interface IAuthService {
 
 export class AuthService {
   static NAME = 'AuthService';
+  config;
+  url;
 
   constructor(
     private $http: ng.IHttpService,
-    private $window: ng.IWindowService
+    private $window: ng.IWindowService,
+    private $httpParamSerializerJQLike: ng.IHttpParamSerializer
   ) {
     'ngInject';
+
+    this.config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+    this.url = 'http://localhost:8080/api'
   }
 
   isLoggedIn() {
     return this.$window.localStorage.getItem('token') !== null;
   }
 
+  login(user) {
+
+    return this.$http.post(
+      this.url + '/login',
+      this.$httpParamSerializerJQLike(user),
+      this.config
+    )
+      .then(response => response.data);
+  }
+
   static factory() {
-    return ($http, $window) => new AuthService($http, $window);
+    return (
+      $http, $window, $httpParamSerializerJQLike
+    ) => new AuthService($http, $window, $httpParamSerializerJQLike);
   }
 
 }
