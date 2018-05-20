afd9bca78715044bfbf4c7aae23632eb19c292f4
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-05-20 22:54:08 +0800

    Remove old parking and payment angular controller
    The route and service are also removed.

--- resources/app/router.js
+++ resources/app/router.js
@@ -9,11 +9,6 @@
         //$locationProvider.html5Mode(true);
         $urlRouterProvider.otherwise('/');
         $stateProvider
-          .state('parking', {
-            url: '/parking',
-            templateUrl: 'pages/parking.html',
-            controller: 'ParkingCtrl as park',
-          })
           .state('seminar', {
             url: '/seminar',
             templateUrl: 'pages/seminar.html'
@@ -23,19 +18,9 @@
             templateUrl: 'pages/transcript.html',
             controller: 'TranscriptCtrl',
           })
-          .state('parking-payment', {
-            url: '/payment/:number',
-            templateUrl: 'pages/payment.html',
-            controller: 'PaymentCtrl as pay'
-          })
           .state('transcript-payment', {
             url: '/payment',
             templateUrl: 'pages/payment.html',
-            controller: 'PaymentCtrl as pay'
-          })
-          .state('success', {
-            url: '/payment.success',
-            templateUrl: 'pages/payment.success.html',
             controller: 'PaymentCtrl as pay'
           });
       });
