4b067ce08958219abd01347ce6fc231400435216
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-04 17:12:13 +0800

    Format webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,113 +4,108 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
+  return {
+    context: path.resolve(__dirname),
 
-    return {
+    entry: {
+      app: './resources/app/index.ts',
+    },
 
-        context: path.resolve(__dirname),
+    output: {
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'public'),
+    },
 
-        entry: {
-            app: [
-                './resources/app/index.ts'
-            ]
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+          },
+        },
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ['ng-annotate-loader', 'ts-loader'],
         },
 
-        output: {
-            filename: '[name].bundle.js',
-            chunkFilename: '[name].bundle.js',
-            path: path.resolve(__dirname, 'public'),
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader:
+                argv.mode === 'development'
+                  ? 'style-loader'
+                  : MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                //https://github.com/zeit/next.js/issues/1325
+                includePaths: ['resources/sass'].map((d) =>
+                  path.join(__dirname, d)
+                ),
+              },
+            },
+          ],
         },
 
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    commons: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendor',
-                        chunks: 'all'
-                    }
-                }
-            }
+        {
+          test: /\.(jpe?g|png|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+              },
+            },
+          ],
         },
 
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    exclude: /node_modules/,
-                    use: [
-                        'ng-annotate-loader',
-                        'ts-loader',
-                    ]
-                },
-
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: argv.mode === 'development' ?
-                                'style-loader' :
-                                MiniCssExtractPlugin.loader
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                url: false
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                //https://github.com/zeit/next.js/issues/1325
-                                includePaths: ['resources/sass']
-                                    .map(d => path.join(__dirname, d))
-                            }
-                        }
-                    ]
-                },
-
-                {
-                    test: /\.(jpe?g|png|gif|ico)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[path][name].[ext]'
-                            }
-                        }
-                    ]
-                },
-
-                {
-                    test: /\.html$/,
-                    use: [
-                        {
-                            loader: 'html-loader'
-                        }
-                    ]
-                }
-            ],
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
         },
+      ],
+    },
 
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'public/index.html')
-            }),
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+      }),
 
-            new MiniCssExtractPlugin({
-                filename: '[name].css',
-                chunkFilename: '[id].css'
-            })
-        ],
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
 
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js']
-        },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
 
-        devServer: {
-            contentBase: path.join(__dirname, "public"),
-            compress: true,
-            port: 8008
-        }
-    }
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      compress: true,
+      port: 8008,
+    },
+  };
 };
