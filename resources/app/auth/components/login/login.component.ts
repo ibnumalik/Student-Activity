5dddd94939975fc9ca5ee22b1f93ec047704b6c2
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-05 15:55:03 +0800

    Refactor login component

--- resources/app/auth/components/login/login.component.ts
+++ resources/app/auth/components/login/login.component.ts
@@ -1,12 +1,18 @@
-import { IWindowService, ITimeoutService } from "angular";
-import { IAuthService, IHttpLoginResponse } from './../../auth';
-import { StateService } from "@uirouter/angularjs";
+import {
+  IWindowService,
+  ITimeoutService,
+  Injectable,
+  IControllerConstructor,
+} from 'angular';
+import { IHttpLoginResponse } from './../../auth';
+import { StateService } from '@uirouter/angularjs';
+import { IAuthService, User } from '../../services/auth.service';
 
 export class LoginComponent implements ng.IComponentOptions {
   static NAME = 'appLogin';
-  public template;
-  public controllerAs;
-  public controller;
+  public template: string;
+  public controllerAs: string;
+  public controller: Injectable<IControllerConstructor>;
 
   constructor() {
     this.template = require('./login.component.html');
@@ -16,9 +22,9 @@ export class LoginComponent implements ng.IComponentOptions {
 }
 
 class LoginController implements ng.IComponentController {
-  private title;
-  private user;
-  private error = false;
+  title = 'Login Page';
+  private user: User;
+  error = false;
 
   constructor(
     private AuthService: IAuthService,
@@ -28,28 +34,30 @@ class LoginController implements ng.IComponentController {
   ) {
     'ngInject';
 
-    this.title = "Login Page";
-    this.user = {};
+    this.user = {
+      email: '',
+      password: '',
+    };
   }
 
   login() {
+    this.AuthService.login(this.user).then((response: IHttpLoginResponse) => {
+      const hasError =
+        response.data.message === 'the email does not exist in database' ||
+        response.data.message === 'wrong password';
 
-    this.AuthService.login(this.user)
-      .then((response: IHttpLoginResponse) => {
+      if (hasError) {
+        return this.showError(hasError);
+      }
 
-        if (
-          response.data.message === 'the email does not exist in database' ||
-          response.data.message === 'wrong password'
-        ) {
-          this.error = true; this.hideError(); return;
-        }
-
-        this.$state.go('app.dashboard.student');
-        this.$window.localStorage.setItem('token', response.data.token);
-      });
+      this.$state.go('app.dashboard.student');
+      this.$window.localStorage.setItem('token', response.data.token);
+    });
   }
 
-  hideError() {
-    this.$timeout(() => this.error = false, 7000);
+  showError(hasError: boolean) {
+    this.error = hasError;
+
+    this.$timeout(() => (this.error = false), 7000);
   }
 }
