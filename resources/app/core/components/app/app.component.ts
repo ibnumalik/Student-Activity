54e0a043b0ba2cc6cef859e1659876f8c9866d4e
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-05 16:02:42 +0800

    Redirect to login or dashboard

--- resources/app/core/components/app/app.component.ts
+++ resources/app/core/components/app/app.component.ts
@@ -1,10 +1,12 @@
-import { IAuthService } from './../../../auth/auth';
 import './app.component.scss';
+import { AuthService } from '../../../auth/services/auth.service';
+import { StateService, TransitionService } from '@uirouter/angularjs';
+import { Injectable, IControllerConstructor } from 'angular';
 
 export class App implements ng.IComponentOptions {
   static NAME: string = 'app';
   template: string;
-  controller: any;
+  controller: Injectable<IControllerConstructor>;
 
   constructor() {
     this.template = require('./app.component.html');
@@ -15,11 +17,47 @@ export class App implements ng.IComponentOptions {
 export class AppController implements ng.IComponentController {
   public isLoggedIn: boolean;
 
-  constructor(private AuthService: IAuthService) {
+  constructor(
+    private $scope: ng.IScope,
+    private AuthService: AuthService,
+    private $state: StateService,
+    private $transitions: TransitionService
+  ) {
     'ngInject';
   }
 
-  $doCheck() {
-    this.isLoggedIn = this.AuthService.isLoggedIn();
+  $onInit() {
+    this.AuthService.isLoggedIn().then((state: boolean) => {
+      this.updateAuthState(state);
+      this.checkCorrectRoute(this.$state);
+    });
+
+    this.$transitions.onStart({ to: 'app' }, (transition) => {
+      const $state = transition.router.stateService;
+
+      this.checkCorrectRoute($state);
+    });
+  }
+
+  /**
+   * Redirect to the correct auth state.
+   */
+  checkCorrectRoute($state: StateService) {
+    if (this.isLoggedIn) {
+      return $state.transitionTo('app.dashboard.student');
+    }
+
+    return $state.transitionTo('auth.login');
+  }
+
+  /**
+   * Listen to `isLoggedIn` event and update state.
+   */
+  updateAuthState(state: boolean) {
+    this.isLoggedIn = state;
+
+    this.$scope.$on('isLoggedIn', (event, state) => {
+      this.isLoggedIn = state;
+    });
   }
 }
