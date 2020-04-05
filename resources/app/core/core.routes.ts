b0d60f8241d32cc5bcad149e46cd913184453708
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-05 15:58:08 +0800

    Format core router file

--- resources/app/core/core.routes.ts
+++ resources/app/core/core.routes.ts
@@ -1,15 +1,16 @@
-import { App } from "./components/app/app.component";
-import { StateProvider, UrlRouterProvider } from "@uirouter/angularjs";
+import { App } from './components/app/app.component';
+import { StateProvider, UrlRouterProvider } from '@uirouter/angularjs';
 
-export const routing =
-($stateProvider: StateProvider, $urlRouterProvider: UrlRouterProvider) => {
+export const routing = (
+  $stateProvider: StateProvider,
+  $urlRouterProvider: UrlRouterProvider
+) => {
   'ngInject';
 
-  $stateProvider
-    .state('app', {
-      url: '/app',
-      component: App.NAME
-    });
+  $stateProvider.state('app', {
+    url: '/app',
+    component: App.NAME,
+  });
 
   $urlRouterProvider.otherwise('/app');
-}
+};
