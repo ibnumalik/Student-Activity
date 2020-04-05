0b1f3b3878a760e6606145ec0927485650c0ec73
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-05 15:57:42 +0800

    Refactor register component

--- resources/app/auth/components/register/register.component.ts
+++ resources/app/auth/components/register/register.component.ts
@@ -1,11 +1,13 @@
-import { IAuthService, IHttpRegisterResponse } from './../../auth';
-import { StateService } from "@uirouter/angularjs";
+import { IHttpRegisterResponse } from './../../auth';
+import { StateService } from '@uirouter/angularjs';
+import { IAuthService, User } from '../../services/auth.service';
+import { Injectable, IControllerConstructor } from 'angular';
 
 export class RegisterComponent implements ng.IComponentOptions {
   static NAME = 'appRegister';
-  public template;
-  public controllerAs;
-  public controller;
+  public template: string;
+  public controllerAs: string;
+  public controller: Injectable<IControllerConstructor>;
 
   constructor() {
     this.template = require('./register.component.html');
@@ -15,8 +17,8 @@ export class RegisterComponent implements ng.IComponentOptions {
 }
 
 class RegisterController implements ng.IComponentController {
-  public title;
-  public user;
+  public title = 'Register Page';
+  public user: User;
   public emailUsed = false;
 
   constructor(
@@ -26,28 +28,30 @@ class RegisterController implements ng.IComponentController {
   ) {
     'ngInject';
 
-    this.title = "Register Page";
-    this.user = {};
+    this.user = {
+      email: '',
+      password: '',
+    };
   }
 
   register() {
-
-    this.AuthService.register(this.user)
-      .then((response: IHttpRegisterResponse) => {
+    this.AuthService.register(this.user).then(
+      (response: IHttpRegisterResponse) => {
 
         if (response.data && response.data.message === 'email already used') {
-          this.emailUsed = true;
-          this.hideError();
-          return;
+          return this.showError();
         }
 
         if (response.status === 'success') {
-          this.$state.go('app.auth.login');
+          this.$state.go('app');
         }
-      });
+      }
+    );
   }
 
-  hideError() {
-    this.$timeout(() => this.emailUsed = false, 7000);
+  showError() {
+    this.emailUsed = true;
+
+    this.$timeout(() => (this.emailUsed = false), 7000);
   }
 }
