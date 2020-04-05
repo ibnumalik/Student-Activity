48e03a6a1f8276978bb45fce7d179e1cbccdcc1b
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-05 23:38:37 +0800

    Format parking component files

--- resources/app/dashboard/components/parking/parking.component.ts
+++ resources/app/dashboard/components/parking/parking.component.ts
@@ -1,43 +1,58 @@
-import './parking.component.scss'
+import './parking.component.scss';
+import { Injectable, IControllerConstructor } from 'angular';
+import { StateService } from '@uirouter/angularjs';
 
 export class ParkingComponent implements ng.IComponentOptions {
-  static NAME:string = 'parkingComponent';
+  static NAME: string = 'parkingComponent';
   public template: string;
-  public controller;
+  public controller: Injectable<IControllerConstructor>;
 
   constructor() {
     this.template = require('./parking.component.html');
-    this.controller = ParkingComponentController
+    this.controller = ParkingComponentController;
   }
 }
 
 export class ParkingComponentController implements ng.IComponentController {
-  public parkingSpaces: any[];
   public groupedParkingSpaces: any[];
   private spaceSelected;
+  private parking;
 
-  constructor(private ParkingService, private $state) { 'ngInject' }
+  constructor(private ParkingService, private $state: StateService) {
+    'ngInject';
+  }
 
   $onInit() {
-    this.ParkingService.getAll()
-      .then(response => {
-        this.parkingSpaces = response.data;
-
-        this.groupParkingBlock();
-      });
+    this.ParkingService.getAll().then((response: ParkingResponse) => {
+      this.groupParkingBlock(response.data);
+    });
   }
 
   continue() {
-    this.$state.go('app.dashboard.parkingPayment', { parkingId: this.spaceSelected.id });
+    this.$state.go('app.dashboard.parkingPayment', {
+      parkingId: this.spaceSelected.id,
+    });
   }
 
-  groupParkingBlock() {
-    this.groupedParkingSpaces = this.parkingSpaces.reduce((r, a) => {
+  groupParkingBlock(parking: Parking[]) {
+    this.parking = parking;
+
+    this.groupedParkingSpaces = parking.reduce((r, a) => {
       r[a.block_name] = r[a.block_name] || [];
       r[a.block_name].push(a);
-
       return r;
     }, Object.create(null));
   }
+}
 
+interface ParkingResponse {
+  status: string;
+  data: Parking[];
+}
+
+interface Parking {
+  id: string;
+  block_name: string;
+  space_number: string;
+  rented: 'false' | 'true';
 }
--- resources/app/dashboard/components/parking/parking.service.ts
+++ resources/app/dashboard/components/parking/parking.service.ts
@@ -1,44 +1,56 @@
+import { environment } from '../../../../environments/environment';
+import { IRequestShortcutConfig } from 'angular';
+
 export class ParkingService {
-    static NAME = 'ParkingService';
-    private url;
-    private config;
+  static NAME = 'ParkingService';
+  private config: IRequestShortcutConfig;
+  url = environment.url;
 
-    constructor(
-        private $http: ng.IHttpService,
-        private $httpParamSerializerJQLike: ng.IHttpParamSerializer
-    ) {
-        'ngInject'
+  constructor(
+    private $http: ng.IHttpService,
+    private $httpParamSerializerJQLike: ng.IHttpParamSerializer
+  ) {
+    'ngInject';
 
-        this.url = 'http://localhost:8080/api';
-        this.config = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-    }
+    this.config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+  }
 
-    getAll() {
-        return this.$http.get(this.url+'/parking')
-            .then(response => response.data);
-    }
+  getAll() {
+    return this.$http
+      .get(this.url + '/parking')
+      .then((response) => response.data);
+  }
 
-    get(id) {
-        return this.$http.get(this.url+'/parking/'+id)
-            .then(response => response.data);
-    }
+  async getAllParking() {
+    const response = await this.$http.get(this.url + '/parking');
 
-    rentSpace(data) {
-        return this.$http.post(
-            this.url+'/parking/rent',
-            this.$httpParamSerializerJQLike(data),
-            this.config
-        ).then(response => response.data);
-    }
+    return response.data;
+  }
 
-    static factory() {
-        return ($http, $httpParamSerializerJQLike) => {
-            'ngInject';
-            return new ParkingService($http, $httpParamSerializerJQLike);
-        }
-    }
+  get(id) {
+    return this.$http
+      .get(this.url + '/parking/' + id)
+      .then((response) => response.data);
+  }
+
+  rentSpace(data) {
+    return this.$http
+      .post(
+        this.url + '/parking/rent',
+        this.$httpParamSerializerJQLike(data),
+        this.config
+      )
+      .then((response) => response.data);
+  }
+
+  static factory() {
+    return ($http, $httpParamSerializerJQLike) => {
+      'ngInject';
+      return new ParkingService($http, $httpParamSerializerJQLike);
+    };
+  }
 }
